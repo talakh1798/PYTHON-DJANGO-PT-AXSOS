692ca48aa9a1a181c74cf18850e675f96e637ac9
talakh1798 <167581630+talakh1798@users.noreply.github.com>
2024-05-29 23:51:31 +0300

    1'st commit

---
+++ Functions Intermediate II/function.py
@@ -0,0 +1,66 @@
+# Update Values in Dictionaries and Lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
+
+print(x)
+print(students)
+print(sports_directory)
+
+# Iterate Through a List of Dictionaries
+def iterateDictionary(students):
+    for student in students:
+        print(f"first_name - {student['first_name']}, last_name - {student['last_name']}")
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students)
+
+# Get Values From a List of Dictionaries
+def iterateDictionary2(key_name, some_list):
+    for dictionary in some_list:
+        print(dictionary[key_name])
+
+students = [
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+# Iterate Through a Dictionary with List Values
+def printInfo(some_dict):
+    for key, value in some_dict.items():
+        print(len(value), key.upper())
+        for item in value:
+            print(item)
+        print()
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+printInfo(dojo)
+
+
+
