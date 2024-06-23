c8b622f3ab65765aefea2011a5852cd4d67105cb
talakh1798 <167581630+talakh1798@users.noreply.github.com>
2024-06-23 15:06:56 +0300

    upload MYsql

---
+++ MySQL Workbench Setup.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (username, email) VALUES ('Tala Khaled', 'talakh17982gmail.com');
+INSERT INTO users (username, email) VALUES ('Tala Khaled', 'talakh17982gmail.com');
+INSERT INTO users (username, email) VALUES ('muhamad Khaled', 'muh56@gmail.com');
+
+SELECT * FROM users
+
+UPDATE users SET username = 'fadi joma', email = 'fadi00@yahoo.com' WHERE id = 2;
+
+DELETE FROM users Where username='Tala khaled'
